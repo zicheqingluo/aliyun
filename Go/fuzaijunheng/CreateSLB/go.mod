ff649370b421ac53580b6f12df7dee5f4ac0b37a
杨小康(10029913) <yangxk-a@glodon.com>
2019-10-16 18:45:12 +0800

    sss

---
+++ Go/fuzaijunheng/CreateSLB/go.mod
@@ -0,0 +1,5 @@
+module aliyun/Go/fuzaijunheng/CreateSLB
+
+go 1.13
+
+require github.com/aliyun/alibaba-cloud-sdk-go v0.0.0-20191016063755-82808b8a45c1 // indirect
---
+++ Go/fuzaijunheng/CreateSLB/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+  	"github.com/aliyun/alibaba-cloud-sdk-go/services/slb"
+  
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
+  
+)
+
+func createListen() {
+	request.ListenerPort = requests.NewInteger(9092)
+	request.Bandwidth = requests.NewInteger(-1)
+	request.LoadBalancerId = "lb-2zex56ck3z5ehxgx1z63l"
+	request.BackendServerPort = requests.NewInteger(9092)
+	request.Scheduler = "wrr"
+	request.PersistenceTimeout = "0"
+	request.EstablishedTimeout = requests.NewInteger(900)
+	request.HealthyThreshold = requests.NewInteger(2)
+	request.UnhealthyThreshold = requests.NewInteger(10)
+  
+	  response, err := client.CreateLoadBalancerTCPListener(request)
+	  if err != nil {
+		  fmt.Print(err.Error())
+	  }
+	  fmt.Printf("配置端口成功,response is %v\n", response)
+}
+
+
+func main() {
+	client, err := slb.NewClientWithAccessKey("cn-beijing", "LTAI4FxeXTRSziZoh9LzZ8VX", "o3Xb4LefYmiDhoiSNYN14ExHGNTRk8")
+
+	request := slb.CreateCreateLoadBalancerTCPListenerRequest()
+	request.Scheme = "https"
+
+
+}
